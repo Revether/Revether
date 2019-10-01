126813fc7a1c096f843ea2d2a48dbd0a6830d058
ofeks10 <ofeks10@gmail.com>
2019-10-01 12:33:42 +0300

    fixed the socket_keepalive to be generic

--- Revether/revether/utils/net.py
+++ Revether/revether/utils/net.py
@@ -1,5 +1,5 @@
 import socket
-
+import sys
 
 def set_socket_keepalive(sock, idle=1, interval=3, fail_count=5):
     """
@@ -13,6 +13,27 @@ def set_socket_keepalive(sock, idle=1, interval=3, fail_count=5):
     """
 
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
-    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPIDLE, idle)
-    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPINTVL, interval)
-    sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPCNT, fail_count)
+
+    # This code is taken from: https://github.com/andymccurdy/redis-py/blob/master/redis/connection.py
+    # detect available options
+    TCP_KEEPCNT = getattr(socket, 'TCP_KEEPCNT', None)
+    TCP_KEEPINTVL = getattr(socket, 'TCP_KEEPINTVL', None)
+    TCP_KEEPIDLE = getattr(socket, 'TCP_KEEPIDLE', None)
+    TCP_KEEPALIVE = getattr(socket, 'TCP_KEEPALIVE', None)
+    SIO_KEEPALIVE_VALS = getattr(socket, 'SIO_KEEPALIVE_VALS', None)
+    if TCP_KEEPIDLE is None and TCP_KEEPALIVE is None \
+            and sys.platform == 'darwin':
+        TCP_KEEPALIVE = 0x10
+
+    # configure
+    if TCP_KEEPCNT is not None:
+        sock.setsockopt(socket.IPPROTO_TCP, TCP_KEEPCNT, fail_count)
+    if TCP_KEEPINTVL is not None:
+        sock.setsockopt(socket.IPPROTO_TCP, TCP_KEEPINTVL, interval)
+    if TCP_KEEPIDLE is not None:
+        sock.setsockopt(socket.IPPROTO_TCP, TCP_KEEPIDLE, idle)
+    elif TCP_KEEPALIVE is not None:
+        sock.setsockopt(socket.IPPROTO_TCP, TCP_KEEPALIVE, idle)
+    elif SIO_KEEPALIVE_VALS is not None:
+        sock.ioctl(SIO_KEEPALIVE_VALS,
+                (1, idle * 1000, interval * 1000))
