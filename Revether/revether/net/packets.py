720445b4563bab664433f2700a230681f759a699
Asaf Zoler <aszoler@gmail.com>
2019-10-01 13:00:30 +0300

    Add connection handshake packet

--- Revether/revether/net/packets.py
+++ Revether/revether/net/packets.py
@@ -2,6 +2,7 @@ import construct
 import enum
 
 LATEST_VERSION = 1
+SHA1_HASH_BYTES_LENGTH = 20
 
 
 class EventType(enum.Enum):
@@ -19,10 +20,24 @@ EventPacket = construct.EmbeddedSwitch(
     }
 )
 
+ConnectionPacket = construct.Struct(
+    'version' / construct.Int8ub,
+    'idb_name' / construct.PascalString(construct.Int16ub, 'utf-8'),
+    'idb_hash' / construct.Bytes(SHA1_HASH_BYTES_LENGTH),
+)
+
 
 def create_event_packet(event_type, *args, **kwargs):
     return EventPacket.build(dict(
         version=LATEST_VERSION,
         event_type=event_type,
         **kwargs
+    ))
+
+
+def create_connection_packet(idb_name, idb_hash):
+    return ConnectionPacket(dict(
+        version=LATEST_VERSION,
+        idb_name=idb_name,
+        idb_hash=idb_hash
     ))
