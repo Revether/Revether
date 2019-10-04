254ec7990e824d3bf1f12a67c1e71428cc767d9a
ofeks10 <ofeks10@gmail.com>
2019-10-04 12:25:52 +0300

    Fixed a bug in the unicoder

--- Revether/revether/utils/unicoder.py
+++ Revether/revether/utils/unicoder.py
@@ -1,12 +1,10 @@
 class Unicoder(object):
     @staticmethod
     def decode(s):
-        if isinstance(s, str):
-            return s
-        return s.decode('raw_unicode_escape')
+        return s.decode('utf-8')
 
     @staticmethod
     def encode(s):
         if isinstance(s, unicode):
             return s
-        return s.encode('raw_unicode_escape')
+        return s.encode('utf-8')
